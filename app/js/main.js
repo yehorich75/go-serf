a3218570c09a5920f678f09976b2e245f439c2cd
Yegorich75 <yegorich75@gmail.com>
2019-11-08 09:44:25 +0200

    slider-map dots

--- app/js/main.js
+++ app/js/main.js
@@ -7,10 +7,10 @@ $(function(){
         cssEase: 'linear',
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="Arrow Left"></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="Arrow Right"></img>',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
@@ -23,6 +23,14 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="Arrow Left"></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="Arrow Right"></img>'
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="Arrow Right"></img>',
+        asNavFor: '.slider-map'
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf-slider'
     })
 });
